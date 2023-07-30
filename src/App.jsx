eb96b876b848cfae414d1c8ce089375da1f151bf
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-07-30 23:09:14 +0530

    Project Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Row from './Components/Row'
+import requests from './request'
 
 function App() {
   return (
-    <div>
-      
+    <div className='app'>
+      <Row title='Netlifx original' fetchUrl={requests.fetchNetflixOriginals}/>
     </div>
   )
 }
--- src/request.js
+++ src/request.js
@@ -1,4 +1,4 @@
-const API_KEY = ""
+const API_KEY = "76deadecb4e996be0639c6b2a624a68c" ;
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
