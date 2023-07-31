361b266057ffcd073e5f16a80ee747641c4c778d
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-07-31 11:41:40 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 import './App.css'
 import Row from './Components/Row/Row'
 import requests from './request'
+import Banner from './Components/Banner/Banner'
 
 function App() {
   return (
     <div className='app'>
-      <Row title='Netlifx original' fetchUrl={requests.fetchNetflixOriginals}/>
+   
+   <Banner/>
+
+   <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow />
+
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
